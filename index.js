8a0b4d9221969543c1d839c0c58fa5820ca5b3a9
rohannegi <rohangovindnegi@yahoo.com>
2021-02-06 21:21:51 -0500

    modified files

---
+++ index.js
@@ -0,0 +1,83 @@
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+const questions = [
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your Github username");
+          return false;
+        }
+        
+    }
+
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?"
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?'
+  }
+];
+
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data), err => { 
+      if (err) throw err;
+      console.log("succesfully created");
+      }
+
+}
+
+
+function init() {
+  inquirer.prompt(questions).then(answers => {
+    writeToFile('README.md', generateMarkdown(answers));
+  });
+}
+
+init();
+
